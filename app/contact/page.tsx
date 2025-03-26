47c0c5298207ff891b708881810cb172b29b224a
Rish Saddiq <rishab.saddiq0607@gmail.com>
2025-03-26 10:15:35 +0000

    add maps

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -115,7 +115,7 @@ export default function ContactPage() {
           <div className="text-center mb-8">
             <h2 className="text-3xl font-bold mb-4 text-[#0b4ea2]">Our Location</h2>
             <p className="text-gray-600 max-w-2xl mx-auto">
-              Visit our office in Bratislava. We&apos;re located in the city center, easily accessible by public transportation.
+              Visit our office in Bratislava. We&apos;re located in the city center, which has  easily accessible by public transportation.
             </p>
 
           </div>
